7845474ad33db5f4a643310448a62830a4eff88b
tjsdn3065 <tjsdn3065@gmail.com>
2025-01-17 14:37:22 +0900

    함수의 기초

---
+++ chap07/calling.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//
+//void simple();
+//
+//int main()
+//{
+//	using namespace std;
+//	simple();
+//	return 0;
+//}
+//
+//void simple()
+//{
+//	using namespace std;
+//	cout << "여기는 simple함수 입니다." << endl;
+//}
---
+++ chap07/protos.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//void cheers(int);
+//double cube(double x);
+//int main()
+//{
+//	using namespace std;
+//	cheers(5);
+//	double side;
+//	cin >> side;
+//	double volume = cube(side);
+//	cout << side << endl;
+//	cout << volume << endl;
+//	cheers(cube(2));
+//	return 0;
+//}
+//
+//void cheers(int n)
+//{
+//	using namespace std;
+//	for (int i = 0;i < n;i++)
+//		cout << "Cheer! ";
+//	cout << endl;
+//}
+//
+//double cube(double x)
+//{
+//	return x * x * x;
+//}
