2aae4e5074b76e7b668239017a458df57f45c8d8
tjsdn3065 <tjsdn3065@gmail.com>
2025-01-17 12:51:43 +0900

    break와 continue 구문

---
+++ jump.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+const int ArSize = 80;
+
+int main()
+{
+	using namespace std;
+	char line[ArSize];
+	int spaces = 0;
+
+	cin.get(line, ArSize);
+	cout << line << endl;
+
+	for (int i = 0;line[i] != '\0';i++)
+	{
+		cout << line[i];
+		if (line[i] == '.')
+			break;
+		if (line[i] != ' ')
+			continue;
+		spaces++;
+	}
+	cout << spaces << endl;
+	return 0;
+}
