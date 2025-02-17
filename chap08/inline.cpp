8843d1888439be843a224d00202579fe924e5ca3
tjsdn3065 <tjsdn3065@gmail.com>
2025-02-18 05:38:39 +0900

    c++ 인라인 함수

---
+++ chap08/inline.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+inline double square(double x)
+{
+	return x * x;
+}
+
+int main()
+{
+	using namespace std;
+	double a, b;
+	double c = 13.0;
+	a = square(5.0);
+	b = square(4.5 + 7.5);
+	cout << a << b << endl;
+	cout << c << endl;
+	cout << square(c++) << endl;
+	cout << c << endl;
+	return 0;
+}
